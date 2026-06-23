a2f0ea712b4fbbb2476d7c795a17fb2b0f0e38e9
Vishal Dange <vishaldange03@gmail.com>
2026-06-23 22:01:45 +0530

    connected to database

---
+++ src/main/java/com/phegon/phegonbank/PhegonbankApplication.java
@@ -0,0 +1,13 @@
+package com.phegon.phegonbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhegonbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhegonbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phegon/phegonbank/PhegonbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phegon.phegonbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhegonbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
